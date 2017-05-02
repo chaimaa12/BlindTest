8ae537a05df8727d254eb31f0dbf24de1bfefa40
Unknown <chaimaa.souri@hotmail.fr>
2017-05-02 15:36:55 +0200

    [MODEL] ajout model

---
+++ BlindTest/Music.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlindTest
+{
+	class Music
+	{
+		public String Titre { get; set; }
+		public String Link { get; set; }
+		public String Genre { get; set; }
+	}
+}
